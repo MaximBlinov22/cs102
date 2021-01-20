65382969d4df6aa4f4f2a8ccf97bd365b9861ac0
DanilZmievskiy <maximblinov2002@gmail.com>
2021-01-20 12:39:04 +0300

    Удалены приватные данные

--- homework05/vkapi/config.py
+++
@@ -1,7 +0,0 @@
-# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
-
-VK_CONFIG = {
-    "domain": "https://api.vk.com/method",
-    "access_token": "fdb003f67419bba8895d541a97b55cc8f3fa6e2f4d08621aae894aeac0fd95ee91f46347e8e485f231b19",
-    "version": "5.126",
-}
